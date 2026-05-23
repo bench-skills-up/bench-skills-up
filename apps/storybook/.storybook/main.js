99e3e255a89f0c3612623bdb6a0761cd6e329b75
rishi-kumar-verma <rishianand001@gmail.com>
2026-05-23 10:47:20 +0530

    chore: add GitHub Pages deployment workflow

--- apps/storybook/.storybook/main.js
+++ apps/storybook/.storybook/main.js
@@ -4,5 +4,11 @@ export default {
     options: {}
   },
   stories: ['../stories/**/*.stories.@(tsx|ts)'],
-  addons: ['@storybook/addon-links', '@storybook/addon-interactions']
+  addons: ['@storybook/addon-links', '@storybook/addon-interactions'],
+  viteFinal: (config) => {
+    return {
+      ...config,
+      base: '/bench-skills-up/storybook/'
+    }
+  }
 }
